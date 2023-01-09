1bf7df782c3f27fe2091a7f1f554904a543fb5a1
jwkim <92710923+jwkim0341@users.noreply.github.com>
2023-01-10 08:48:57 +0900

    [mod] 정적경로 수정

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// //static 마운트 지정
-// app.use('/static', express.static(path.resolve(__dirname, 'frontend', 'static')));
+//static 마운트 지정
+app.use(express.static(path.join(__dirname, "frontend/static")));
 
-// //get요청시 index.html 접속
-// app.get('/*', (req, res) => {
-//   console.log(123);
-//   // res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
-// });
-
-// //포트번호 3000 지정 및 서버 실행
-// app.listen(process.env.PORT || 5500, () => console.log('Server running!'));
-app.use('/static', express.static(path.resolve(__dirname, 'frontend', 'static')));
-
-app.get('/*', (req, res) => {
-  console.log(req);
-  res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
+//get요청시 index.html 접속
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server running...'));
+//포트번호 3000 지정 및 서버 실행
+app.listen(process.env.PORT || 5500, () => console.log("Server running!"));
